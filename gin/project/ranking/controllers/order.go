d0648e6c90aa60700bac3a07ef2c206da8a7bee3
awsl1414 <3030994569@qq.com>
2024-07-09 16:04:05 +0800

    :sparkles: (gin/project/ranking/) JSON传参的两种方式

--- gin/project/ranking/controllers/order.go
+++ gin/project/ranking/controllers/order.go
@@ -2,7 +2,7 @@
  * @Author: awsl1414 3030994569@qq.com
  * @Date: 2024-07-09 11:27:33
  * @LastEditors: awsl1414 3030994569@qq.com
- * @LastEditTime: 2024-07-09 13:31:05
+ * @LastEditTime: 2024-07-09 16:02:31
  * @FilePath: /go-learn/gin/project/ranking/controllers/order.go
  * @Description:
  *
@@ -14,8 +14,25 @@ import "github.com/gin-gonic/gin"
 
 type OrderController struct{}
 
+type Search struct {
+	Cid  int    `json:"cid"`
+	Name string `json:"name"`
+}
+
 func (o OrderController) GetList(c *gin.Context) {
-	cid := c.PostForm("cid")
-	name := c.DefaultPostForm("name", "zhangsan")
-	ReturnSuccess(c, 0, cid, name, 1)
+	// param := make(map[string]interface{})
+	// err := c.BindJSON(&param)
+
+	search := &Search{}
+
+	err := c.BindJSON(&search)
+
+	if err != nil {
+		ReturnError(c, 4001, gin.H{"err": err})
+		return
+	}
+
+	// ReturnSuccess(c, 0, param["cid"], param["name"], 1)
+	ReturnSuccess(c, 0, search.Cid, search.Name, 1)
+
 }
