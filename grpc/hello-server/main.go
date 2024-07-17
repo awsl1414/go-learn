0e7c20ed56afe6d9b5f9246141fcb2d03ec8b8e4
awsl1414 <3030994569@qq.com>
2024-07-17 23:06:26 +0800

    :sparkles: (grpc/hello-server/main.go) 服务端tls认证实现

--- grpc/hello-server/main.go
+++ grpc/hello-server/main.go
@@ -2,7 +2,7 @@
  * @Author: awsl1414 3030994569@qq.com
  * @Date: 2024-07-14 10:39:28
  * @LastEditors: awsl1414 3030994569@qq.com
- * @LastEditTime: 2024-07-14 16:26:15
+ * @LastEditTime: 2024-07-17 23:05:28
  * @FilePath: /go-learn/grpc/hello-server/main.go
  * @Description:
  *
@@ -16,6 +16,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type server struct {
@@ -27,11 +28,14 @@ func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloR
 }
 
 func main() {
+
+	creds, _ := credentials.NewServerTLSFromFile("grpc/key/test.pem", "grpc/key/test.key")
+
 	// 开启端口
 	listen, _ := net.Listen("tcp", "127.0.0.1:9090")
 
 	// 创建grpc服务
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 
 	// 在grpc服务端注册自己编写的服务
 	pb.RegisterSayHelloServer(grpcServer, &server{})
