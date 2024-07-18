d58505c6809098897ade52fd5f3780b599f1fe81
awsl1414 <3030994569@qq.com>
2024-07-18 10:10:06 +0800

    :sparkles: (grpc/hello-client(server)/main.go) 增加自定义token验证

--- grpc/hello-client/main.go
+++ grpc/hello-client/main.go
@@ -2,7 +2,7 @@
  * @Author: awsl1414 3030994569@qq.com
  * @Date: 2024-07-14 10:39:22
  * @LastEditors: awsl1414 3030994569@qq.com
- * @LastEditTime: 2024-07-17 23:32:44
+ * @LastEditTime: 2024-07-18 10:03:02
  * @FilePath: /go-learn/grpc/hello-client/main.go
  * @Description:
  *
@@ -19,13 +19,36 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-func main() {
+type PerRPCCredentials interface {
+	GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error)
+	RequireTransportSecurity() bool
+}
 
+type ClientTokenAuth struct{}
+
+func (c ClientTokenAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appId":  "awsl1414",
+		"appKey": "123456",
+	}, nil
+}
+
+func (c ClientTokenAuth) RequireTransportSecurity() bool {
+	return true
+}
+
+func main() {
+	// tls 认证
 	creds, _ := credentials.NewClientTLSFromFile("../key/test.pem", "*.sailor.work") // serverNameOverride 应从浏览器获取
 
-	// 连接server端，此处禁用安全传输，没有加密和验证 grpc.Dial() 已弃用
+	//连接到 server 端，此处启用安全传输
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(creds))
+	opts = append(opts, grpc.WithPerRPCCredentials(new(ClientTokenAuth)))
+
+	// 连接 server 端，此处禁用安全传输，没有加密和验证 grpc.Dial() 已弃用
 	// conn, err := grpc.NewClient("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	conn, err := grpc.NewClient("127.0.0.1:9090", grpc.WithTransportCredentials(creds))
+	conn, err := grpc.NewClient("127.0.0.1:9090", opts...)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
--- grpc/hello-server/main.go
+++ grpc/hello-server/main.go
@@ -2,7 +2,7 @@
  * @Author: awsl1414 3030994569@qq.com
  * @Date: 2024-07-14 10:39:28
  * @LastEditors: awsl1414 3030994569@qq.com
- * @LastEditTime: 2024-07-17 23:15:34
+ * @LastEditTime: 2024-07-18 10:08:28
  * @FilePath: /go-learn/grpc/hello-server/main.go
  * @Description:
  *
@@ -11,24 +11,47 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	pb "hello-server/proto"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 )
 
 type server struct {
 	pb.UnimplementedSayHelloServer
 }
 
+// 业务
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	// 获取元数据
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("未传输token")
+	}
+	var appId string
+	var appKey string
+
+	// 元数据是放在http头传输的，http头的键是小写的
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+	}
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+	}
+
+	if appId != "awsl1414" || appKey != "123456" {
+		return nil, errors.New("token不正确")
+	}
+
 	return &pb.HelloResponse{ResponseMsg: "hello " + req.RequestName}, nil
 }
 
 func main() {
-
+	// tls 认证
 	creds, _ := credentials.NewServerTLSFromFile("../key/test.pem", "../key/test.key")
 
 	// 开启端口
